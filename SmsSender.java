73dfe1b35a01b1a2cb7e7e0145b42073dd8f83a6
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2026-08-04 01:19:18 +0530

    Delete SmsSender.java

--- SmsSender.java
+++
@@ -1,9 +0,0 @@
-package NotificationSystem;
-
-public class SmsSender implements NotificationSender {
-
-    @Override
-    public void sendNotification(Notification notification) {
-        System.out.println("Sending SMS: " + notification.getContext());
-    }
-}
