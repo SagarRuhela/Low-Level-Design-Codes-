e85abfc9430a38ccf2999e602eeede9de16ef084
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2026-08-04 01:17:14 +0530

    Delete EmailSender.java

--- EmailSender.java
+++
@@ -1,9 +0,0 @@
-package NotificationSystem;
-
-public class EmailSender implements NotificationSender {
-
-    @Override
-    public void sendNotification(Notification notification) {
-        System.out.println("Sending Email: " + notification.getContext());
-    }
-}
